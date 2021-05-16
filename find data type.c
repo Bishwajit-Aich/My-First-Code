a96449accf66dd386f23db6b93eb1c2b3e1d24df
Bishwajit Aich <84278738+bishwajit870@users.noreply.github.com>
2021-05-16 21:39:45 +0600

    Add files via upload

---
+++ find data type.c
@@ -0,0 +1,23 @@
+/*write a program find 
+the size Data Type*/
+
+#include<stdio.h>
+int main()
+{
+    int i;
+    float f;
+    double d;
+    char c;
+    
+    printf("size of int : %d\n",sizeof(i));
+    printf("size of float : %d\n",sizeof(f));
+    printf("size of double : %d\n",sizeof(d));
+    printf("size of character : %d\n",sizeof(c));
+
+
+
+
+
+
+return 0;
+}
